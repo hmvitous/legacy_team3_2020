724bb11d2c68b95d8e9ecd2d501189828fc4f9ad
Emma_Thalen <thlalen.emma@gmail.com>
2020-02-12 22:43:25 +0100

    trying to figure out the factorybot and how to use it from the course documentation from monday, not passing any tests yet

--- spec/features/user_can_send_message_spec.rb
+++ spec/features/user_can_send_message_spec.rb
@@ -1,16 +1,21 @@
 require 'rails_helper'
+require 'pry-byebug'
 
 feature 'User can create' do
-        it do
+        before do
             visit conversation_path
-            
+            click_on "Login"
+            fill_in "user_email", with: "name@mail.com"
+            fill_in "user_password", with: "12345678"
+            click_on "Log in"
         end
         
-    # context 'an email' do
-    #     #click_on 'Compose'
-    #     select(recipients[])
-    #     fill_in 'Subject', with: 'Hello'
-    #     fill_in 'Message', with: 'Test Message'
+    context 'an email' do
+        click_on 'Compose'
+        binding.pry
+        select(recipients[])
+        fill_in 'Subject', with: 'Hello'
+        fill_in 'Message', with: 'Test Message'
 
-    # end 
+    end 
 end
--- spec/models/conversation_spec.rb
+++ spec/models/conversation_spec.rb
@@ -1,8 +1,14 @@
 require 'rails_helper'
 
 RSpec.describe ConversationsController, type: :controller do
-    describe 'DB table' do
-        it { is_expected.to have_db_column :id}
-        it { is_expected.to have_db_column :subject}
+    # describe 'DB table' do
+    #     it { is_expected.to have_db_column :id}
+    #     it { is_expected.to have_db_column :subject}
+    # end
+
+    describe 'Factory' do
+        it 'should have valid Factory' do
+            expect(FactoryBot.create(:user)).to be_valid
+        end
     end
 end
