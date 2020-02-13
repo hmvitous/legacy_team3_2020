c3f89f7dc755fc9499fbe6c3d57db53d9ef193af
Hunter <hmvitous@yahoo.com>
2020-02-13 12:05:06 +0100

    Modifying acceptance for creating/sending message. Not passing yet but making progress

--- spec/features/user_can_send_message_spec.rb
+++ spec/features/user_can_send_message_spec.rb
@@ -3,19 +3,19 @@ require 'pry-byebug'
 
 feature 'User can create' do
         before do
-            visit conversation_path
-            click_on "Login"
+            visit user_session_path
             fill_in "user_email", with: "name@mail.com"
             fill_in "user_password", with: "12345678"
             click_on "Log in"
         end
         
-    context 'an email' do
-        visit conversation_path
-        binding.pry
-        select(recipients[])
+    it 'an email' do
+        click_on 'Inbox'
+        click_on 'Compose'
+        # choose '#'
         fill_in 'Subject', with: 'Hello'
         fill_in 'Message', with: 'Test Message'
+        click_on "Send Message"
 
     end 
 end
